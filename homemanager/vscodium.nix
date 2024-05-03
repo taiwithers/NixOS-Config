e488234a40aa7024422953326f6ae1f4498a168a
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-03 16:25:55 -0400

    2024-05-03 16:25 : id 45 -> /nix/store/z6rqhxqqadhv9pcixxn5sc3hmv5sl43m-home-manager-generation

--- homemanager/vscodium.nix
+++ homemanager/vscodium.nix
@@ -11,30 +11,41 @@
     mutableExtensionsDir = true;
     extensions = with pkgs.vscode-extensions;
       [
-        njpwerner.autodocstring
-        vscodevim.vim
-        ms-python.isort
-        ms-python.python
-        ms-toolsai.jupyter
-        ms-pyright.pyright
-        kamadorueda.alejandra
-        jellyedwards.gitsweep
-        gruntfuggly.todo-tree
-        file-icons.file-icons
-        ms-python.vscode-pylance
-        mechatroner.rainbow-csv
-        johnpapa.vscode-peacock
-        james-yu.latex-workshop
-        yzhang.markdown-all-in-one
-        ms-vscode-remote.remote-ssh
-        vscode-icons-team.vscode-icons
+        # njpwerner.autodocstring
+        # vscodevim.vim
+        # ms-python.isort
+        # ms-python.python
+        # ms-toolsai.jupyter
+        # ms-pyright.pyright
+        # kamadorueda.alejandra
+        # jellyedwards.gitsweep
+        # gruntfuggly.todo-tree
+        # file-icons.file-icons
+        # ms-python.vscode-pylance
+        # mechatroner.rainbow-csv
+        # johnpapa.vscode-peacock
+        # james-yu.latex-workshop
+        # yzhang.markdown-all-in-one
+        # ms-vscode-remote.remote-ssh
+        # vscode-icons-team.vscode-icons
       ]
       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        # additional extensions that aren't in nixpkgs
+        # follow instructions here: https://matthewrhone.dev/nixos-vscode
+        # install extensions manually through codium
+        # run the script: https://github.com/NixOS/nixpkgs/blob/42d815d1026e57f7e6f178de5a280c14f7aba1a5/pkgs/misc/vscode-extensions/update_installed_exts.sh
+        # paste outputs for any extensions that aren't above into this area
         {
-          name = "vscode-eslint";
-          publisher = "dbaeumer";
-          version = "2.1.14";
-          sha256 = "113w2iis4zi4z3sqc3vd2apyrh52hbh2gvmxjr5yvjpmrsksclbd";
+          name = "moegi-theme";
+          publisher = "ddiu8081";
+          version = "0.7.1";
+          sha256 = "0s9rymrif1dq7py1mr5fhlz53sa6xr57ssia88l0lq39dq02j845";
+        }
+        {
+          name = "vscode-powertools";
+          publisher = "egomobile";
+          version = "0.67.4";
+          sha256 = "0wl86x8rq6dssjcj74b9xr2bpjicdaciva24kvckqpfmxmslylz7";
         }
       ];
 
