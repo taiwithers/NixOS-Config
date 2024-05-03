95bab175a3763638286027defc7512429253aba5
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-03 13:33:40 -0400

    2024-05-03 13:33 : id 32 -> /nix/store/zj5wf50xs3l9hy0rhx63nsq5frhmws50-home-manager-generation

--- rebuild.sh
+++ rebuild.sh
@@ -4,13 +4,32 @@
 # A rebuild script that commits on a successful build
 set -e
 
-action=$1
+case $1 in 
+  'nix')
+    directory=~/.config/nixfiles/system
+    build() {
+      sudo nixos-rebuild switch -I nixos-config=configuration.nix &>nixos-switch.log || (bat nixos-switch.log | rg error && exit 1)
+      current=$(nixos-rebuild list-generations | rg current)
+    }
+  ;;
 
-# Edit your config
-# $EDITOR configuration.nix
+  'home')
+    directory=~/.config/nixfiles/homemanager
+    build() {
+      home-manager -f home.nix switch &>nixos-switch.log || (bat nixos-switch.log | rg error && exit 1)
+      current=$(home-manager generations | sed -n 1p)
+    }
+  ;;
+
+  *)
+    echo "Usage: bash rebuild.sh [nix|home]" && exit 1
+  ;;
+
+esac
+
 
 # cd to your config dir
-pushd ~/.config/nixfiles
+pushd $directory
 
 # Early return if no changes were detected (thanks @singiamtel!)
 if git diff --quiet ^HEAD '*.nix'; then
@@ -26,32 +45,7 @@ alejandra . &>/dev/null \
 # Shows your changes
 git diff -U0 '*.nix'
 
-case $action in 
-  'nix')
-    echo "NixOS Rebuilding..."
-
-    # Rebuild, output simplified errors, log tracebacks
-    sudo nixos-rebuild switch -I nixos-config=configuration.nix &>nixos-switch.log || (bat nixos-switch.log | rg error && exit 1)
-
-    # Get current generation metadata
-    current=$(nixos-rebuild list-generations | rg current)
-  ;;
-
-  'home')
-    echo "Home Manager Rebuilding..."
-
-    # Rebuild, output simplified errors, log tracebacks
-    home-manager -f home.nix switch &>nixos-switch.log || (bat nixos-switch.log | rg error && exit 1)
-
-    # Get current generation metadata
-    current=$(home-manager generations | sed -n 1p)
-  ;;
-
-  *)
-    echo "Usage: bash rebuild.sh [nix|home]" && exit 1
-  ;;
-esac
-
+build 
 
 # Commit all changes with the generation metadata
 git commit -am "$current"
