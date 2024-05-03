2cc737404d6c79a238a50690b9f1deacce5f117e
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-03 13:13:53 -0400

    31 current  2024-05-03 13:13:49  23.11.6759.0638fe2715d9  6.1.88                          *

--- gnome.nix
+++ gnome.nix
@@ -6,5 +6,13 @@
   environment.gnome.excludePackages = with pkgs; [
     gnome-connections # remote desktop
     gnome.geary # mail app
+    gnome.yelp # help viewer
+    gnome.gnome-calendar
+    gnome.gnome-characters
+    gnome.gnome-contacts
+    gnome.gnome-logs
+    gnome.gnome-maps
+    gnome.gnome-music
+    gnome.gnome-weather
   ];
 }
