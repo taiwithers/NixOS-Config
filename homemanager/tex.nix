04078940bfd48991b01ce0115932cf8934aa9a37
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-06 17:24:13 -0400

    2024-05-06 17:24 : id 62 -> /nix/store/sgc8gbjbjrp4kxncg4d4i534fj6px201-home-manager-generation

--- homemanager/tex.nix
+++ homemanager/tex.nix
@@ -4,11 +4,19 @@
   ...
 }: let
   # small is required over minimal for pdflatx which can't be installed as a package
-  mytex = pkgs.texliveSmall.withPackages (ps:
-    with ps; [
+  mytex = pkgs.texlive.combine {
+    inherit
+      (pkgs.texlive)
+      scheme-small
       latexmk
       standalone
-    ]);
+      ;
+  };
+  # mytex = pkgs.texliveSmall.withPackages (ps:
+  #   with ps; [
+  #     latexmk
+  #     standalone
+  #   ]);
 in {
   home.packages = [mytex];
 }
