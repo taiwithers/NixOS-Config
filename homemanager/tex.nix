abafb0f079ffe9534086f01c37930133b1de6e38
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-06 17:25:09 -0400

    2024-05-06 17:25 : id 63 -> /nix/store/jy43cw8sd7lavsmjz396gjqlfvl0bbl5-home-manager-generation

--- homemanager/tex.nix
+++ homemanager/tex.nix
@@ -3,13 +3,13 @@
   pkgs,
   ...
 }: let
-  # small is required over minimal for pdflatx which can't be installed as a package
   mytex = pkgs.texlive.combine {
     inherit
       (pkgs.texlive)
-      scheme-small
+      scheme-small # small is required over minimal for pdflatex which can't be installed as a package
       latexmk
       standalone
+      revtex
       ;
   };
   # mytex = pkgs.texliveSmall.withPackages (ps:
