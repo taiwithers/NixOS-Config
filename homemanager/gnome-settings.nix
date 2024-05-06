df46f73256570dec46033d56ab695e1b72bcd69a
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-06 16:29:17 -0400

    2024-05-06 16:29 : id 57 -> /nix/store/fm3qhs0g2mdfdf0w6r2b248xklhvbnq4-home-manager-generation

--- homemanager/gnome-settings.nix
+++ homemanager/gnome-settings.nix
@@ -49,8 +49,19 @@
 
     "org/gnome/desktop/interface" = {
       color-sceme = "prefer-dark";
-      enable-hot-corners = false;
+      show-battery-percentage = true;
+      overlay-scrolling = true;
+      locate-pointer = true;
     };
+
+    "org/gnome/settings-daemon/plugins/power".power-button-action = "interactive";
+
+    # multitasking
+    "org/gnome/desktop/interface".enable-hot-corners = false;
+    "org/gnome/mutter".edge-tiling = true;
+    "org/gnome/mutter".dynamic-workspaces = true;
+    "org/gnome/mutter".workspaces-only-on-primary = false;
+    "org/gnome/shell/app-switcher".current-workspace-only = true;
 
     # media and accessibility keybindings
     "org/gnome/settings-daemon/plugins/media-keys" = {
@@ -63,6 +74,7 @@
 
     # window managment keybindings
     "org/gnome/desktop/wm/keybindings" = {
+      # under navigation
       move-to-monitor-down = [];
       move-to-monitor-left = [];
       move-to-monitor-right = [];
@@ -72,6 +84,56 @@
       move-to-workspace-last = [];
       move-to-workspace-1 = [];
       move-to-workspace-2 = [];
+      switch-panels = []; # "switch system controls"
+      switch-panels-backward = []; # ??
+      cycle-panels = []; # "switch system controls directly"
+      cycle-panels-backward = []; # ??
+      switch-to-workspace-last = [];
+      switch-to-workspace-1 = [];
+      switch-to-workspace-left = [];
+      switch-to-workspace-right = [];
+      cycle-windows = []; # "switch windows directly"
+      cycle-windows-backward = []; # ??
+      cycle-group = []; # "switch windows of an app directly"
+      cycle-group-backward = [];
+      switch-group = []; # "switch windows of an application"
+      switch-group-backward = [];
+
+      # under window section
+      activate-window-menu = [];
+      minimize = ["<Super>Down"];
+      unmaximize = [];
+      toggle-maximized = [];
+    };
+
+    # screenshot keybindings
+    "org/gnome/shell/keybindings" = {
+      show-screen-recording-ui = []; # "record a screencast interactively"
+      screenshot = ["<Super>Print"]; # take a screenshot
+      show-screenshot-ui = ["Print"]; # take a screenshot interactively
+      screenshot-window = [];
+    };
+
+    # system
+    "org/gnome/shell/keybindings".focus-active-notification = [];
+    "org/gnome/shell/keybindings".toggle-quick-settings = [];
+    "org/gnome/shell/keybindings".restore-shortcuts = [];
+    "org/gnome/shell/keybindings".toggle-application-view = [];
+    "org/gnome/shell/keybindings".toggle-message-tray = [];
+    "org/gnome/shell/keybindings".switch-input-source = [];
+    "org/gnome/shell/keybindings".switch-input-source-backward = [];
+    "org/gnome/settings-daemon/plugins/media-keys".logout = [];
+
+    # custom keybindings
+    "org/gnome/desktop/wm/keybindings" = {
+      custom-keybindings = [
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Open Nautilus";
+      command = "nautilus";
+      binding = "<Super>e";
     };
   };
 }
