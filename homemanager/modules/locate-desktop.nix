9909870cd8903ff40f79d4c066d3f8b652c3402d
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-10 14:25:30 -0400

    2024-05-10 14:11 : id 43 -> /nix/store/gbz0kj6dz3l6h5iy3dc3g1qk1j5j8pj9-home-manager-generation

--- homemanager/modules/locate-desktop.nix
+++ homemanager/modules/locate-desktop.nix
@@ -4,9 +4,12 @@ if builtins.hasAttr "desktopItem" pkg
 then "${pkg.pname}.desktop"
 else let
   # search for any files in the pkg.outPath/share/applications directory, catch for directory not existing
-  filelist =
-    builtins.tryEval (builtins.attrNames (builtins.readDir "${pkg}/share/applications"));
+  directory = "${pkg}/share/applications";
+  fileList =
+    if builtins.pathExists directory
+    then builtins.attrNames (builtins.readDir directory)
+    else [];
 in
-  if filelist.success && builtins.length filelist.value > 0
-  then builtins.elemAt filelist.value 0
+  if builtins.length fileList > 0
+  then builtins.elemAt fileList 0
   else null
