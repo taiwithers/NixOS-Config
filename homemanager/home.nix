bb3bd3be8ecafb60641f8efebc57a74330f6cc56
Tai Withers <59430904+taiwithers@users.noreply.github.com>
2024-05-11 19:10:32 -0400

    add MIME types

--- homemanager/home.nix
+++ homemanager/home.nix
@@ -110,13 +110,46 @@ in {
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "inode/directory" = ["org.kde.dolphin.desktop"];
+        "application/gzip" = []; # .gz , .tgz
+        "application/json" = []; # .json
         "application/pdf" = ["firefox.desktop"];
-        "text/html" = ["firefox.desktop"];
+        "application/zip-compressed" = []; # .zip
+        "application/x-debian-package" = []; # .deb, .udeb
+        "application/x-font-ttf" = []; # .ttc, .ttf
+        "application/x-shellscript" = []; # .sh
+        "application/x-tar" = []; # .tar
+        "application/x-tex" = []; # .tex
         "application/xhtml+xml" = ["firefox.desktop"];
+        "application/yaml" = []; # yaml, yml
+        
+        "application/vnd.oasis.opendocument.graphics" = []; # .odg
+        "application/vnd.oasis.opendocument.graphics-template" = []; # .otg
+        "application/vnd.oasis.opendocument.presentation" = []; # .odp
+        "application/vnd.oasis.opendocument.presentation-template" = []; # .otp
+        "application/vnd.oasis.opendocument.spreadsheet" = []; # .ods
+        "application/vnd.oasis.opendocument.spreadsheet-template" = []; # .ots
+        "application/vnd.oasis.opendocument.text" = []; # .odt
+        "application/vnd.oasis.opendocument.text-template" = []; # .ott
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation" = []; # .pptx
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = []; # .docx
+        
+        "image/gif" = []; # .gif
+        "image/ico" = []; # .ico
+        "image/jpeg" = []; # .jpeg
+        "image/png" = []; # .png
+        "image/webp" = []; # .webp
+
+        "inode/directory" = ["org.kde.dolphin.desktop"];
+        
+        "text/calendar" = []; # .ics .ifb
+        "text/csv" = []; # .csv
+        "text/html" = ["firefox.desktop"];
+        "text/plain" = []; # .conf, .def, .diff, .in, .ksh, .list, .log, .pl, .text, .txt
+        "text/x-markdown" = []; # .md, .markdown, .mdown, .markdn
+        "text/x-py" = []; # .py
+        
         "x-scheme-handler/http" = ["firefox.desktop"];
         "x-scheme-handler/https" = ["firefox.desktop"];
-
         "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
         "x-scheme-handler/zoom-mtg" = ["zoom.desktop"]; # not sure if correct
       };
@@ -142,6 +175,7 @@ in {
     "org/gnome/shell" = {
       # taskbar apps
       favorite-apps = map (pkg: locateDesktop pkg) taskbar-pkgs;
+      user-theme.name = "";
     };
 
     "org/gnome/desktop/interface" = {
@@ -149,6 +183,7 @@ in {
       show-battery-percentage = true;
       overlay-scrolling = true;
       locate-pointer = true;
+      cursor-theme = "";
     };
 
     "org/gnome/settings-daemon/plugins/power".power-button-action = "interactive";
