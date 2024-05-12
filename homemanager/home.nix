5b1ff2fdd069d108fb52cb5a5fcd8cba33055dc0
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-12 15:28:22 -0400

    2024-05-12 15:28 : id 125 -> /nix/store/5rsrl77f72d6yzimjma8n64gdp29bi2n-home-manager-generation

--- homemanager/home.nix
+++ homemanager/home.nix
@@ -48,7 +48,8 @@
     # nix-colors = import inputs.nix-colors.homeManagerModules.default;
     # colours = nix-colors."${name}";
     names = [
-      "rose-pine"
+      "zenbones"
+      "rose-pine" # purple
       "qualia" # black + pastels
       "porple" # more washed out
 
