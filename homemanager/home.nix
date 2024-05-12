36b65b3f6893d5c79b0460cfa83be0eaa4d18664
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-12 12:23:25 -0400

    2024-05-12 12:23 : id 70 -> /nix/store/0w2y2n9hlf3qvy2rs60kii5ypkvnrxvk-home-manager-generation

--- homemanager/home.nix
+++ homemanager/home.nix
@@ -236,11 +236,19 @@ in {
 
   # Themeing
 
-  # download selected base 16 theme to tilix theme directory
-  xdg.configFile."${config.xdg.configHome}/tilix/schemes/base16-${colourScheme}.json".source = pkgs.fetchurl {
+  # get profile string with `dconf dump /com/gexperts/Tilix/profiles`
+  # dconf.settings."com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d" = {};
+
+  home.file."testoutput".source = pkgs.fetchurl {
     url = "https://github.com/karlding/base16-tilix/blob/master/tilix/base16-${colourScheme}.json";
     hash = "sha256-UEit9XxoZUmcb5ZRnYYWt9Pe1V+nD20Yl4klGOLwiSY=";
   };
+
+  # download selected base 16 theme to tilix theme directory
+  # xdg.configFile."${config.xdg.configHome}/tilix/schemes/base16-${colourScheme}.json".source = pkgs.fetchurl {
+  #   url = "https://github.com/karlding/base16-tilix/blob/master/tilix/base16-${colourScheme}.json";
+  #   hash = "sha256-UEit9XxoZUmcb5ZRnYYWt9Pe1V+nD20Yl4klGOLwiSY=";
+  # };
 
   # download all themes for sublime
   # xdg.configFile."${config.xdg.configHome}/sublime-text/Packages/User/Base16".source = pkgs.fetchFromGithub {
