a14acd0893f149b34d283baf8a740c5b1d2a3346
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-13 14:30:41 -0400

    2024-05-13 14:30 : id 143 -> /nix/store/lcn1lpcfbxn7f5y2bcnhcywxc1zhhb3d-home-manager-generation

--- homemanager/modules/locate-desktop.nix
+++ homemanager/modules/locate-desktop.nix
@@ -8,13 +8,14 @@ in
     # search for any files in the pkg.outPath/share/applications directory, catch for directory not existing
     directory = "${pkg}/share/applications"; # directory to search
     fileIsDesktop = filename: lib.strings.hasSuffix ".desktop" filename;
+    stringIsShorter = a: b: builtins.lessThan (builtins.stringLength a) (builtins.stringLength b);
     fileList =
       if builtins.pathExists directory
       then builtins.filter fileIsDesktop (builtins.attrNames (builtins.readDir directory)) # get files in directory ending in ".desktop"
       else [];
   in
     if builtins.length fileList > 0
-    then builtins.elemAt fileList 0 # return first .desktop file found (if any)
+    then builtins.head (builtins.sort stringIsShorter fileList) # return last .desktop file found (if any)
     else throw "Cannot locate .desktop file for package ${pkg.pname}"
 # throw error if none found
 
