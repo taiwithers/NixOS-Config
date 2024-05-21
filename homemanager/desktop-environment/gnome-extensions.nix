04adcd3aa32a50aa1e2b4f425575d66ed6ec71ae
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-21 17:58:55 -0400

    2024-05-21 17:51 : id 170 -> /nix/store/g08mc4zclp7n2hlnlmbrjmzr4jj2mx46-home-manager-generation

--- homemanager/desktop-environment/gnome-extensions.nix
+++ homemanager/desktop-environment/gnome-extensions.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  # tidalwm = import ()
+  t = pkgs.desktops.gnome.extensions.buildGnomeExtension.nix;
 in rec {
   home.packages = with pkgs.gnomeExtensions; [
     all-windows
@@ -17,22 +17,6 @@ in rec {
 
   dconf.settings."org/gnome/shell" = {
     disable-user-extensions = false;
-
-    # access with: gnome-extensions list
     enabled-extensions = map (ext: ext.extensionUuid) home.packages;
-    # [
-    #   "extension-list@tu.berry"
-    #   "favourites-in-appgrid@harshadgavali.gitlab.org"
-    #   "gTile@vibou"
-    #   "start-overlay-in-application-view@Hex_cz"
-    #   "AlphabeticalAppGrid@stuarthayhurst"
-    #   "click-to-close-overview@l3nn4rt.github.io"
-    #   "ding@rastersoft.com"
-    #   "steal-my-focus-window@steal-my-focus-window"
-    #   "appindicatorsupport@rgcjonas.gmail.com"
-    #   "all-windows@ezix.org"
-    #   "dash-to-panel@jderose9.github.com"
-    #   "forge@jmmaranan.com"
-    # ];
   };
 }
