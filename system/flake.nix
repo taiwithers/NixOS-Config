4e4f0abde84f46b9fb6b29c9574c9c8e3285de01
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-22 19:23:24 -0400

    9 current   2024-05-22 19:17:50  23.11.20240506.27c1399   6.1.90                          *

--- system/flake.nix
+++ system/flake.nix
@@ -3,15 +3,19 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "github:hyprwm/Hyprland?submodules=1";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
+    ...
+  } @ inputs: let
     pkgs = import nixpkgs;
   in {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
       modules = [./configuration.nix];
     };
   };
