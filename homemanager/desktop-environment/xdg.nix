b905b668c098fefcf17d72158847ed0d0adcf4a5
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-22 15:09:48 -0400

    2024-05-22 15:09 : id 177 -> /nix/store/csk34j18njjnzldnc2b06md93rvp7q45-home-manager-generation

--- homemanager/desktop-environment/xdg.nix
+++ homemanager/desktop-environment/xdg.nix
@@ -1,28 +1,6 @@
 # TODO: clean up MIME types
 {pkgs, ...}: let
   locateDesktop = import ../../nix-scripts/locate-desktop.nix;
-
-  preferredPackages = with pkgs; {
-    browsers = [
-      firefox
-    ];
-
-    imageviewers = [
-      loupe
-      firefox
-    ];
-
-    texteditors = [
-      unstable.sublime4
-      vscodium-fhs
-      gnome.gedit
-    ];
-
-    filebrowsers = [
-      libsForQt5.dolphin
-    ];
-  };
-
   getDesktopFiles = list: map (p: "${locateDesktop p}") list;
 
   defaults = builtins.mapAttrs (name: value: (getDesktopFiles value)) (with pkgs; {
@@ -45,15 +23,6 @@
       libsForQt5.dolphin
     ];
   });
-  # defaults = builtins.mapAttrs (group: packageList: getDesktopFiles) preferredPackages;
-  # browser_desktops = locateDesktop pkgs.firefox;
-  # imageviewer = locateDesktop pkgs.loupe;
-  # texteditors = map (p: "${locateDesktop p}") (with pkgs; [
-  #   unstable.sublime4
-  #   vscodium-fhs
-  #   gnome.gedit
-  # ]);
-  # filebrowser = locateDesktop pkgs.libsForQt5.dolphin;
 in {
   xdg = {
     enable = true;
@@ -65,40 +34,40 @@ in {
       defaultApplications = {
         "application/gzip" = []; # .gz , .tgz
         "application/json" = defaults.texteditors; # .json
-        # "application/pdf" = [browser];
-        # "application/zip-compressed" = []; # .zip
-        # "application/x-debian-package" = []; # .deb, .udeb
-        # "application/x-font-ttf" = []; # .ttc, .ttf
-        # "application/x-shellscript" = texteditors; # .sh
-        # "application/x-tar" = []; # .tar
-        # "application/x-tex" = texteditors; # .tex
-        # "application/xhtml+xml" = [browser];
-        # "application/yaml" = texteditors; # yaml, yml
+        "application/pdf" = defaults.browsers;
+        "application/zip-compressed" = []; # .zip
+        "application/x-debian-package" = []; # .deb, .udeb
+        "application/x-font-ttf" = []; # .ttc, .ttf
+        "application/x-shellscript" = defaults.texteditors; # .sh
+        "application/x-tar" = []; # .tar
+        "application/x-tex" = defaults.texteditors; # .tex
+        "application/xhtml+xml" = defaults.browsers;
+        "application/yaml" = defaults.texteditors; # yaml, yml
 
-        # "application/vnd.oasis.opendocument.graphics" = []; # .odg
-        # "application/vnd.oasis.opendocument.graphics-template" = []; # .otg
-        # "application/vnd.oasis.opendocument.presentation" = []; # .odp
-        # "application/vnd.oasis.opendocument.presentation-template" = []; # .otp
-        # "application/vnd.oasis.opendocument.spreadsheet" = []; # .ods
-        # "application/vnd.oasis.opendocument.spreadsheet-template" = []; # .ots
-        # "application/vnd.oasis.opendocument.text" = []; # .odt
-        # "application/vnd.oasis.opendocument.text-template" = []; # .ott
-        # "application/vnd.openxmlformats-officedocument.presentationml.presentation" = []; # .pptx
-        # "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = []; # .docx
+        "application/vnd.oasis.opendocument.graphics" = []; # .odg
+        "application/vnd.oasis.opendocument.graphics-template" = []; # .otg
+        "application/vnd.oasis.opendocument.presentation" = []; # .odp
+        "application/vnd.oasis.opendocument.presentation-template" = []; # .otp
+        "application/vnd.oasis.opendocument.spreadsheet" = []; # .ods
+        "application/vnd.oasis.opendocument.spreadsheet-template" = []; # .ots
+        "application/vnd.oasis.opendocument.text" = []; # .odt
+        "application/vnd.oasis.opendocument.text-template" = []; # .ott
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation" = []; # .pptx
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = []; # .docx
 
-        # "image/*" = [imageviewer];
+        "image/*" = defaults.imageviewers;
 
-        # "inode/directory" = [filebrowser];
+        "inode/directory" = defaults.filebrowsers;
 
-        # "text/calendar" = []; # .ics .ifb
-        # "text/csv" = []; # .csv
-        # "text/html" = [browser];
-        # "text/plain" = texteditors; # .conf, .def, .diff, .in, .ksh, .list, .log, .pl, .text, .txt
-        # "text/x-markdown" = texteditors; # .md, .markdown, .mdown, .markdn
-        # "text/x-py" = texteditors; # .py
+        "text/calendar" = []; # .ics .ifb
+        "text/csv" = []; # .csv
+        "text/html" = defaults.browsers;
+        "text/plain" = defaults.texteditors; # .conf, .def, .diff, .in, .ksh, .list, .log, .pl, .text, .txt
+        "text/x-markdown" = defaults.texteditors; # .md, .markdown, .mdown, .markdn
+        "text/x-py" = defaults.texteditors; # .py
 
-        "x-scheme-handler/http" = ["firefox.desktop"];
-        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/http" = defaults.browsers;
+        "x-scheme-handler/https" = defaults.browsers;
         "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
         "x-scheme-handler/zoom-mtg" = ["zoom.desktop"]; # not sure if correct
       };
