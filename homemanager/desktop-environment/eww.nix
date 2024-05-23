c6a8baf33a596ce13409529f38023830050fecb3
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-23 16:36:32 -0400

    2024-05-23 16:29 : id 193 -> /nix/store/h43f7k8ykq91srhmgcgxpj1wab506054-home-manager-generation

---
+++ homemanager/desktop-environment/eww.nix
@@ -0,0 +1,21 @@
+{config, ...}: {
+  xdg.configFile."${config.xdg.configHome}/eww/" = {
+    "eww.yuck".text = ''
+      (defwindow example
+             :monitor 0
+             :geometry (geometry :x "0%"
+                                 :y "20px"
+                                 :width "90%"
+                                 :height "30px"
+                                 :anchor "top center")
+             :stacking "fg"
+             :reserve (struts :distance "40px" :side "top")
+             :windowtype "dock"
+             :wm-ignore false
+      "example content")
+
+    '';
+
+    "eww.css".text = '''';
+  };
+}
