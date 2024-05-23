ef91f6f86162984795bfcb0520c771af36a9dcca
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-23 16:35:22 -0400

    create wrapper for nix-search

---
+++ bash-scripts/nix-search-wrapper.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+if [ -z $1 ]; then 
+	echo "Missing search term"
+	exit 1
+fi
+
+system="x86_64-linux"
+stable="23.11"
+unstable="unstable"
+
+stableResultsFile="/tmp/stableResults.json"
+unstableResultsFile="/tmp/unstableResults.json"
+
+# ANSI codes
+OSC="\u001B]" # operating system command
+BELL="\u0007"
+LINK_BUFFER="8;;"
+END_FMT="\e[0m"
+link_style="\e[35m" # foreground magenta
+stable_style="\e[32m" # foreground green
+unstable_style="\e[34m" # foreground blue
+programs_style=
+
+
+getChannelSearchResults () {
+	# echo "Searching channel $1 with args ${@:2}"
+	local results="$(nix-search --channel=$1 --json ${@:2})"
+	
+	# remove newlines and add commas
+	results=$(echo "$results" | sd "\n\{" ",{")
+
+	# wrap with square brackets
+	results="[$(echo $results)]"
+
+	# extract information with jq
+	results=$(echo $results | jq  "[.[] | select(.package_platforms | contains([\"$system\"])) | {
+													name: .package_attr_name,
+												 	versions: {\"$1\":.package_pversion},
+												 	homepage: .package_homepage,
+												 	programs: .package_programs
+												}] ")
+
+	echo "$results"
+}
+makeLink () { echo "${OSC}${LINK_BUFFER}${2}${BELL}${1}${OSC}${LINK_BUFFER}${BELL}"; }
+removeQuotes() { echo $1 | sd '^"' '' | sd '"$' ''; }
+getJSONValue () { echo $1 | jq ".[0].$2"; }
+
+# save search results to file so they can be piped through jq again
+echo $(getChannelSearchResults $stable $@) > $stableResultsFile
+echo $(getChannelSearchResults $unstable $@) > $unstableResultsFile
+
+# merge results from search
+merged=$(jq  "add | . * (input | .[])" $stableResultsFile $unstableResultsFile)
+
+# run through each result
+(echo $merged | jq -c "[.]") | while read item; do		
+
+	# extract information
+	name=$(removeQuotes $(getJSONValue $item "name"))
+	url=$(removeQuotes $(getJSONValue $item "homepage.[0]"))
+	stableVersion=$(removeQuotes $(getJSONValue $item "versions.\"$stable\""))
+	unstableVersion=$(removeQuotes $(getJSONValue $item "versions.\"$unstable\""))
+	provides=$(echo $(getJSONValue $item "programs") | sd "\n" "" | sd -F "[ " "[" | sd -F " ]" "]" | sd '"' '')
+
+	# add formatting/colours
+	nameString="${link_style}$(makeLink $name $url)${END_FMT}"
+	stableString="${stable}:${stable_style}${stableVersion}${END_FMT}"
+	unstableString="${unstable}:${unstable_style}${unstableVersion}${END_FMT}"
+	providesString=""
+	if [ ${#provides} -gt 2 ]; then providesString="-> ${programs_style}${provides}${END_FMT}"; fi
+
+	# print :)
+	echo -e "$nameString $stableString $unstableString $providesString"
+done
