4668e2bd8e766357d50e3873e53d1d10b4cdc07b
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-23 10:06:07 -0400

    2024-05-23 10:06 : id 185 -> /nix/store/ll0da2wvw21ppq2phf8f8nyf04jmqxk6-home-manager-generation

--- homemanager/desktop-environment/hyprland.nix
+++ homemanager/desktop-environment/hyprland.nix
@@ -11,9 +11,19 @@
       bind = [
         "$mainMod, M, exit" # super + M : exit hyprland
         "$mainMod, E, exec, $fileManager" # super + E : open filemanager
+        "$mainMod, I, exec, gnome-control-center"
       ];
 
       "disable_hyprland_logo" = true;
+
+      # on startup
+      exec-once = [
+        ''          swaybg \
+                      --mode=fill \
+                      --image=/run/current-system/sw/share/background/gnome/adwaita-d.jpg''
+      ];
     };
   };
 }
+# swaybg --mode fill -image /run/current-system/sw/share/background/gnome/adwaita-d.jpg
+
