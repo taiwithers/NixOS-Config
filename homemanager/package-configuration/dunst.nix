1f4f86e456591ee17c1c268ee8de1be0afc715e8
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-24 16:48:21 -0400

    2024-05-24 16:44 : id 204 -> /nix/store/rlcp7gn3v7jbiq9ki44gm2m32wj79j2f-home-manager-generation

--- homemanager/package-configuration/dunst.nix
+++ homemanager/package-configuration/dunst.nix
@@ -5,15 +5,18 @@
   services.dunst.settings = {
     global = {
       monitor = 0;
+      notification_limit = 0; # 0 -> no limit
+      show_indicators = "yes";
 
       # geometry/positioning
       origin = "top-right";
       offset = "10x50";
 
-      notification_limit = 0; # 0 -> no limit
+      # aesthetics
       separator_height = 2; # pixels between notifications
       frame_width = 3; # window frame width
       frame_color = "#aaaaaa";
+      corner_radius = 0;
     };
   };
 }
