deefb19bed7b8c8cb1f7b9ad8b578a5836c9bc60
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-05-24 20:39:09 -0400

    2024-05-24 20:39 : id 210 -> /nix/store/dlhwmbm7xqvb2jxp6prs9cb3ll0x86wd-home-manager-generation

--- homemanager/desktop-environment/eww.nix
+++ homemanager/desktop-environment/eww.nix
@@ -6,25 +6,28 @@
     (defwindow example
            :monitor 0
            :geometry (geometry :x "0%"
-                               :y "20px"
-                               :width "90%"
+                               :y "0%"
+                               :width "100%"
                                :height "30px"
                                :anchor "top center")
            :stacking "fg"
            :exclusive true
-           :focusable true
-    (greeter :text "Hi"
-             :name "Tai"))
-
-
-    (defwidget greeter [?text name]
-      (box :orientation "horizontal"
-           :halign "center"
-        text
-        (button :onclick "dunstify 'Hello' 'Hello, ''${name}'"
-          "Greet")))
-
+           :focusable false
+      topbar)
+    (defwidget topbar
+      box :orientation "horizontal"
+          :halign "right"
+          EWW_TIME
+    )
   '';
+  # (greeter :text "Hi"
+  #          :name "Tai"))
+  # (defwidget greeter [?text name]
+  #   (box :orientation "horizontal"
+  #        :halign "center"
+  #     text
+  #     (button :onclick "dunstify 'Hello' 'Hello, ''${name}'"
+  #       "Greet")))
 
   xdg.configFile."${config.xdg.configHome}/eww/eww.css".text = '''';
 }
