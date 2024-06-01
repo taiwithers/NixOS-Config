3d426d31497964620f22b2ed5d05b5197772dc7d
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-01 10:39:15 -0400

    2024-06-01 10:39 : id 233 -> /nix/store/bfv4hz2z748p50k734kap4vjivqhlqzj-home-manager-generation

--- homemanager/desktop-environment/hyprland-startup.sh
+++ homemanager/desktop-environment/hyprland-startup.sh
@@ -3,10 +3,8 @@
 swaybg --mode=fill --image=/run/current-system/sw/share/backgrounds/gnome/adwaita-d.jpg &
 
 eww daemon
-eww open notification-window
-# eww open topbar-window
-# eww open bottombar-window
-
+eww open topbar-window
+eww open bottombar-window
 
 # dunstify "sending notification w/ dunstify"
 
