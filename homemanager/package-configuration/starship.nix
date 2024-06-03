0b84cb853585f915c7b2b8ed5f1454693b42a3a2
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-02 20:00:52 -0400

    2024-06-02 20:00 : id 253 -> /nix/store/mcq7l74qg6x6p9cla62j8068pn39bkdj-home-manager-generation

--- homemanager/package-configuration/starship.nix
+++ homemanager/package-configuration/starship.nix
@@ -4,5 +4,6 @@
   programs.starship.enable = true;
 
   programs.starship.settings = {
+    format = "$username$hostname$shlvl$singularity$directory$vcsh$git_branch$git_state$git_metrics$package$python$nix_shell$conda$direnv$env_var$custom$sudo$jobs$battery$time$status$os$container$shell$character";
   };
 }
