2badd7978dc98770b8ec3a5b49c642bb18db2537
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-02 20:11:14 -0400

    2024-06-02 20:11 : id 254 -> /nix/store/7643mwi87qx7jfh2f4dnwb6pm3kfzn4p-home-manager-generation

--- homemanager/package-configuration/starship.nix
+++ homemanager/package-configuration/starship.nix
@@ -5,5 +5,22 @@
 
   programs.starship.settings = {
     format = "$username$hostname$shlvl$singularity$directory$vcsh$git_branch$git_state$git_metrics$package$python$nix_shell$conda$direnv$env_var$custom$sudo$jobs$battery$time$status$os$container$shell$character";
+
+    battery = {
+      format = "[$symbol$percentage]($style)";
+      display.threshold = 50;
+      display.style = "bold red";
+    };
+
+    character = {
+      # enter text prompt, shows output of last command
+      format = "$symbol";
+      success_symbol = "[❯](bold green)";
+      error_symbol = "[❯](bold red)";
+    };
+
+    cmd_duration = {
+      min_time = 500;
+    };
   };
 }
