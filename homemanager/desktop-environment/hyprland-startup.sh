b615c23ee42aa91d28fbfc7b966ca6d2c9aca8a4
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-02 22:54:57 -0400

    2024-06-02 22:54 : id 290 -> /nix/store/5v93220zhaxk5jcgxy3dm73d3hn4cjka-home-manager-generation

--- homemanager/desktop-environment/hyprland-startup.sh
+++ homemanager/desktop-environment/hyprland-startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-swaybg --mode=fill --image=/run/current-system/sw/share/backgrounds/gnome/adwaita-d.jpg &
+swaybg --mode=fill --image=/run/current-system/sw/share/backgrounds/gnome/fold-l.jxl &
 
 eww kill
 eww daemon
