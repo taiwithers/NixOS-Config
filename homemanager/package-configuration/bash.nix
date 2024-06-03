18d3c7d5c15dfe1b64f833992b3b3a9bcb8533a0
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-02 21:31:33 -0400

    2024-06-02 21:31 : id 273 -> /nix/store/5p4qqf8iwya8hc3gryiyq856avg92d9v-home-manager-generation

--- homemanager/package-configuration/bash.nix
+++ homemanager/package-configuration/bash.nix
@@ -2,7 +2,7 @@
   programs.bash = {
     enable = true; # apply home.shellAliases to bash
     historyFile = "${config.xdg.stateHome}/bash/history"; # clean up homedir
-    bashrcExtra = "export ICEAUTHORITY=\"\${XDG_CACHE_HOME}\"/ICEauthority\n # ${config.home.profileDirectory}";
+    bashrcExtra = "export ICEAUTHORITY=\"\${XDG_CACHE_HOME}\"/ICEauthority";
     # bashrcExtra = "export HISTFILE=\"\${XDG_STATE_HOME}\"/bash/history";
   };
 }
