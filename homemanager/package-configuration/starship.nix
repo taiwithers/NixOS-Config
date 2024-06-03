20d667816cbfaef04e5f2f217f3a43b4ef2c48f5
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-02 20:37:11 -0400

    2024-06-02 20:37 : id 257 -> /nix/store/c7sa797d6bbx6jjzv0k8fzdw187rwigd-home-manager-generation

--- homemanager/package-configuration/starship.nix
+++ homemanager/package-configuration/starship.nix
@@ -4,7 +4,26 @@
   programs.starship.enable = true;
 
   programs.starship.settings = {
-    format = "$username$hostname$shlvl$singularity$directory$vcsh$git_branch$git_state$git_metrics$package$python$nix_shell$conda$direnv$env_var$custom$sudo$jobs$battery$time$status$os$container$shell$character";
+    format = ''      $username$hostname\
+                    $battery\
+                    $conda\
+                    $custom\
+                    $directory\
+                    $git_branch$git_state$git_metrics\
+                    $jobs\
+                    $nix_shell\
+                    $os\
+                    $package\
+                    $python\
+                    $shell\
+                    $shlvl\
+                    $singularity\
+                    $status\
+                    $sudo\
+                    $time\
+                    $vcsh\
+                    $character
+    '';
 
     battery = {
       format = "[$symbol$percentage]($style)";
@@ -22,8 +41,6 @@
           style = "bold red";
         }
       ];
-      # display.threshold = 50;
-      # display.style = "bold red";
     };
 
     character = {
@@ -35,7 +52,71 @@
 
     cmd_duration = {
       min_time = 500;
-      format = "";
+      format = "took [$duration]($style) ";
+      style = "white";
+      show_notifications = true;
+      min_time_to_notify = 60;
+    };
+
+    conda = {
+      style = "dimmed green";
+      format = "[$symbol$environment]($style)";
+      ignore_base = true;
+    };
+
+    directory = {
+      format = "[$path]($style)[$read_only]($read_only_style)";
+      style = "bold cyan";
+      home_symbol = "~";
+
+      truncation_length = 3;
+      turncate_to_repo = true;
+      truncation_symbol = ".../";
+
+      # read_only = ""; # copy/pasting emoji into sublime doesn't work too well
+      read_only_style = "red";
+    };
+
+    git_branch = {
+      format = "on [$symbol$branch(:$remote_branch)]($style) ";
+      style = "dimmed purple";
+      ignore_branches = ["master" "main"];
+    };
+
+    git_state = {
+      style = "bold yellow";
+      format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+    };
+
+    git_metrics = {
+      added_style = "bold green"; # style for number of added lines
+      deleted_style = "bold red"; # style for number of deleted lines
+      only_nonzero_diffs = true;
+      format = "([+$added]($added_style) )([-$deleted]($deleted_style) )";
+      disabled = false;
+      ignore_submodules = true; # ignore changes to submodules
+    };
+
+    git_status = {
+      format = "([\[$all_status$ahead_behind\]]($style) )";
+      style = "bold red";
+      ignore_submodules = true;
+      conflicted = "x";
+      ahead = "⇡$count";
+      behind = "⇣$count";
+      diverged = "⇕⇡$ahead_count⇣$behind_count";
+      up_to_date = "";
+      untracked = "?";
+      stashed = "$";
+      modified = "!";
+      staged = "+";
+      renamed = "»";
+      deleted = "✘";
+    };
+
+    hostname = {
+      ssh_only = true;
+      ssh_symbol = "\udb82\udcb9 ";
     };
   };
 }
