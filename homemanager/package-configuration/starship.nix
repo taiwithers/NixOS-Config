5e817057e6317f13bcd0472825b82c892ff69385
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-04 20:11:53 -0400

    2024-06-04 20:11 : id 320 -> /nix/store/lnxkfbic0la72wd380j6fwm9x4qfli90-home-manager-generation

--- homemanager/package-configuration/starship.nix
+++ homemanager/package-configuration/starship.nix
@@ -12,7 +12,7 @@ in {
     format = "$username$hostname$battery$conda$custom$directory$git_branch$git_state$git_metrics$jobs$nix_shell$status$sudo$character";
     right_format = "$time";
 
-    continuation_prompt = "> ";
+    continuation_prompt = "\t❯ ";
 
     # modules
     battery = {
