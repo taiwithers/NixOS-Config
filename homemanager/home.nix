62ceb59d6e11c6bb0f205385e99e7591e080706f
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-07 11:37:40 -0400

    2024-06-07 11:37 : id 351 -> /nix/store/hgi7awfb6hfb9vh228jgbn9j7sm77kvh-home-manager-generation

--- homemanager/home.nix
+++ homemanager/home.nix
@@ -72,7 +72,7 @@ in {
   sops = {
     defaultSopsFile = "${config.xdg.configHome}/sops/secrets/example-secrets.yaml";
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
-    secrets.example-key = {
+    secrets.example_key = {
       path = "%r/example-key.txt";
     };
     validateSopsFiles = false; # do not require sops files to be in the nix store
