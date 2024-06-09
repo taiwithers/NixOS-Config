bd8c4a1a68fd3adb6cf87a13734f71386b191472
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-09 13:54:13 -0400

    Refactor file structure

---
+++ homemanager/pkgs/gnome/default.nix
@@ -0,0 +1,7 @@
+{...}: {
+  imports = [
+    ./gnome.nix
+    ./gnome-extensions.nix
+    ./gnome-keybinds.nix
+  ];
+}
---
+++ homemanager/pkgs/sops/sops.nix
@@ -0,0 +1,24 @@
+{config, ...}: let
+  # setup sops and age
+  # mkdir --parents ~/.config/sops/age
+  # age-keygen --output ~/.config/sops/age/keys.txt
+  # to get public key:
+  # age-keygen -y ~/.config/sops/age/keys.txt
+  # setup secrets
+  # sops ~/.config/NixOS-Config/secrets/secrets.yaml
+in {
+  sops = {
+    defaultSopsFile = "${config.xdg.configHome}/NixOS-Config/secrets/secrets.yaml";
+    defaultSopsFormat = "yaml";
+    age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+
+    secrets = {
+      group_hostname = {};
+      group_username = {};
+    };
+  };
+
+  home.activation.custom-sops-nix = let
+    systemctl = config.systemd.user.systemctlPath;
+  in "${systemctl} --user reload-or-restart sops-nix";
+}
