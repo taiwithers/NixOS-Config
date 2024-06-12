9acc7247f770d2b89a4c0b12745d5152f0e55d85
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-12 13:33:12 -0400

    2024-06-12 13:33 : id 428 -> /nix/store/7n6zpxq7madsj19pvmljfzvxc56485f1-home-manager-generation

--- homemanager/pkgs/superfile/superfile.nix
+++ homemanager/pkgs/superfile/superfile.nix
@@ -5,7 +5,7 @@
 }: let
   spfDir = "${config.xdg.configHome}/superfile";
   colours = theme-config.app-themes.superfile;
-  chroma-highlighting-theme = "trac";
+  chroma-highlighting-theme = "vulcan";
   # api, base16-snazzy, catppuccin-mocha, gruvbox, monokai
   # https://github.com/alecthomas/chroma/tree/master/styles
 in {
