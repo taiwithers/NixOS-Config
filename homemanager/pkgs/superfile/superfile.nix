9553a9e4a5358bd7d7f16745841edd9673fcbbc2
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-12 13:28:28 -0400

    2024-06-12 13:28 : id 413 -> /nix/store/idhqzq66xghggr4rzv0mmm19psylzihf-home-manager-generation

--- homemanager/pkgs/superfile/superfile.nix
+++ homemanager/pkgs/superfile/superfile.nix
@@ -5,8 +5,8 @@
 }: let
   spfDir = "${config.xdg.configHome}/superfile";
   colours = theme-config.app-themes.superfile;
-  chroma-highlighting-theme = "modus-operandi";
-  # api, base16-snazzy, catppuccin-mocha, gruvbox
+  chroma-highlighting-theme = "modus-vivendi";
+  # api, base16-snazzy, catppuccin-mocha, gruvbox, monokai
   # https://github.com/alecthomas/chroma/tree/master/styles
 in {
   home.shellAliases."spf" = "superfile";
