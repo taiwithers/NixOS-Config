7bf2a629d3e68641eb7d25a7a045f58a91755243
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-12 15:10:16 -0400

    2024-06-12 15:10 : id 449 -> /nix/store/akvvlhgxpvh97bzasihwahfdrlv3qg8b-home-manager-generation

--- homemanager/pkgs/python/python.nix
+++ homemanager/pkgs/python/python.nix
@@ -23,18 +23,18 @@ in {
     # clean up ~
     export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
   '';
-  programs.bash.initExtra = ''
-    conda_active="$(conda 2>&1 /dev/null)"
-    if [[ $? -eq 127 ]]; then
-      conda-shell
-      echo "activating conda"
-    else
-      echo "not activating conda"
-    fi
-  '';
+  #   programs.bash.initExtra = ''
+  #     conda_active="$(conda 2>&1 /dev/null)"
+  #     if [[ $? -eq 127 ]]; then
+  #       conda-shell
+  #       echo "activating conda"
+  #     else
+  #       echo "not activating conda"
+  #     fi
+  #   '';
 
-  programs.bash.logoutExtra = ''
-    echo "running conda deactivate"
-    conda deactivate
-  '';
+  #   programs.bash.logoutExtra = ''
+  #     echo "running conda deactivate"
+  #     conda deactivate
+  #   '';
 }
