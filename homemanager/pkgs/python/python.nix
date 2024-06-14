7b3705c4b3d0c9351686129f09a5ef523f1d3f36
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-14 11:16:09 -0400

    2024-06-14 11:16 : id 472 -> /nix/store/2w1g5yk4gjc0854az6y0gp90b8ch2wcb-home-manager-generation

--- homemanager/pkgs/python/python.nix
+++ homemanager/pkgs/python/python.nix
@@ -1,24 +1,8 @@
 {...}: let
-  conda-init = ''
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    # ...and home-manager and myself
-    __conda_setup="$('/home/tai/.conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-    	eval "$__conda_setup"
-    else
-    	if [ -f "home/tai/.conda/etc/profile.d/conda.sh" ]; then
-    		. "/home/tai/.conda/etc/profile.d/conda.sh"
-    	else
-    		export PATH="/home/tai/.conda/bin:$PATH"
-    	fi
-    fi
-    unset __conda_setup
-    # <<< conda initialize <<<
-  '';
+  condainit = builtins.readFile ./conda-init.sh;
 in {
   programs.bash.bashrcExtra = ''
-    # ${conda-init}
+    ${condainit}
 
     # clean up ~
     export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
