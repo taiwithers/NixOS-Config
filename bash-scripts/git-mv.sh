006e4cf37edc0d3d70d19775d3f8a407c037d8d9
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-17 15:38:33 -0400

    write script to select mv/git mv

---
+++ bash-scripts/git-mv.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+source=$1
+destination=$2
+
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+if [ "$inside_git_repo" ]; then
+  git mv $source $destination
+else
+  mv $source $destination
+fi
