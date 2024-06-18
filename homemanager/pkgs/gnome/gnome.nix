5862530f5fbaa83b3edce8ad8dddcbed2ed40874
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-18 16:56:11 -0400

    2024-06-18 16:56 : id 515 -> /nix/store/z1mdyc7bk60rcpmb05vzzf90w6g184sq-home-manager-generation

--- homemanager/pkgs/gnome/gnome.nix
+++ homemanager/pkgs/gnome/gnome.nix
@@ -60,6 +60,7 @@
     enable = true;
     theme.name = "Adwaita-dark";
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
 
   home.sessionVariables.GTK_THEME = "adwaita-dark";
