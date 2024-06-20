dbfd835bc811a92e0d26a1f36b763b0a799f56ea
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-20 12:38:25 -0400

    2024-06-20 12:38 : id 7 -> /nix/store/4p1m9wvkfj99n5hkk3nnkdjamqws2hl6-home-manager-generation

--- homemanager/pkgs/superfile/superfile.nix
+++ homemanager/pkgs/superfile/superfile.nix
@@ -9,10 +9,33 @@
   # base16-snazzy, catppuccin-mocha, gruvbox, monokai, vulcan
   # https://github.com/alecthomas/chroma/tree/master/styles
 in {
-  home.shellAliases."spf" = "superfile";
+  # home.shellAliases."spf" = "superfile";
+  programs.bash.bashrcExtra = ''
+    spf() {
+        os=$(uname -s)
+
+        # Linux
+        if [[ "$os" == "Linux" ]]; then
+            export SPF_LAST_DIR="''${XDG_STATE_HOME:-$HOME/.local/state}/superfile/lastdir"
+        fi
+
+        # macOS
+        if [[ "$os" == "Darwin" ]]; then
+            export SPF_LAST_DIR="$HOME/Library/Application Support/superfile/lastdir"
+        fi
+
+        command spf "$@"
+
+        [ ! -f "$SPF_LAST_DIR" ] || {
+            . "$SPF_LAST_DIR"
+            rm -f -- "$SPF_LAST_DIR" > /dev/null
+        }
+    }
+  '';
 
   xdg.configFile."${spfDir}/config.toml".source = ./config.toml;
   xdg.configFile."${spfDir}/hotkeys.toml".source = ./hotkeys.toml;
+
   xdg.configFile."${spfDir}/theme/base16.toml".text = ''
     code_syntax_highlight = "${chroma-highlighting-theme}"
 
