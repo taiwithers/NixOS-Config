1b7789400b1360c85d36c6b0183bd57080bf2426
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-28 18:36:33 -0400

    2024-06-28 18:36 : id 61 -> /nix/store/0x2d37f5sgh824k5mllw45d5lzhbr1pi-home-manager-generation

--- homemanager/pkgs/gaia.nix
+++ homemanager/pkgs/gaia.nix
@@ -6,6 +6,8 @@
   programs.bash.bashrcExtra = ''
     # GAIA
     export STARLINK_DIR=$(dirname $(dirname $(readlink $(which starversion))))
+    shopt -u expand-aliases
     source $STARLINK_DIR/etc/profile
+    shopt -s expand-aliases
   '';
 }
