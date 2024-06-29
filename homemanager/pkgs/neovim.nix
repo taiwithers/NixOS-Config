76b723755c1d58534a5ac39c11960eecaa646471
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-29 16:00:01 -0400

    2024-06-29 16:00 : id 67 -> /nix/store/vnz8gm8l0kxz05pqncjzgbawggzb1lf0-home-manager-generation

--- homemanager/pkgs/neovim.nix
+++ homemanager/pkgs/neovim.nix
@@ -1,5 +1,24 @@
 {...}: {
   programs.neovim = {
     enable = true;
+    defaultEditor = true;
+    extraConfig = "";
+    plugins = [];
+
+    # extraLuaConfig = "";
+    # extraPackages = [];
+    # extraLuaPackages = luaPkgs: [];
+    # extraPython3Packages = pyPkgs: [];
+    # extraWrapperArgs = [];
+    # generatedConfigs = {};
+    # generatedConfigViml = {};
+
+    viAlias = false;
+    vimAlias = false;
+    vimdiffAlias = false;
+
+    withNodeJs = false;
+    withPython3 = false;
+    withRuby = false;
   };
 }
