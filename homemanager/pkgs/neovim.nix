dd2b55b53d9f732ab8dfb028be0b03191caa6c4f
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-29 19:17:36 -0400

    2024-06-29 19:17 : id 76 -> /nix/store/49l26fjjl9lgjg1s4c6bl2z1wdfnzb2m-home-manager-generation

--- homemanager/pkgs/neovim.nix
+++ homemanager/pkgs/neovim.nix
@@ -16,12 +16,16 @@
           pylsp.enable = true;
         };
       };
-      # multicursors.enable = true; # https://github.com/smoka7/multicursors.nvim
+
       nvim-tree = {
         # https://github.com/nvim-tree/nvim-tree.lua
         enable = true;
         openOnSetup = true;
       };
+
+      which-key.enable = true;
+
+      multicursors.enable = true; # https://github.com/smoka7/multicursors.nvim
       # otter.enable = true; # https://github.com/jmbuhr/otter.nvim/
       # statuscol.enable = true; # https://github.com/luukvbaal/statuscol.nvim/
       # surround.enable = true; # https://github.com/tpope/vim-surround/
@@ -34,7 +38,6 @@
       # treesitter-refactor.enable = true;
       # trim.enable = true; # https://github.com/cappyzawa/trim.nvim/
       # undotree.enable = true; # https://github.com/mbbill/undotree/
-      which-key.enable = true;
     };
 
     clipboard.providers.wl-copy.enable = true;
