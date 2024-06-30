1c62b78859c6d12bb41cf5e2ccfa99995ff265b9
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-06-30 15:17:04 -0400

    vastly improve get-package-dir

--- scripts/get-package-dir.sh
+++ scripts/get-package-dir.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
 
+getBinaryDirectory() {
+		echo $(dirname $(dirname $(readlink $1)))
+}
+
 if [ -z $1 ]; then 
 	echo "No package given!"
 	exit 1
-else
-	linkedDirectory="$(which $1 2> /dev/null)"
-	if [[ $? -eq 0 ]]; then
-		echo $(dirname $(dirname $(readlink $linkedDirectory)))
-		allLinkedDirectories="$(which --all $1 2> /dev/null)"
-		echo $allLinkedDirectories
-	else
-		echo "Can't find package $1 in PATH"
-	fi
 fi
+
+# get all potentials (or try to)
+allLinkedDirectories="$(which --all $1 2> /dev/null)"
+if [[ $? -eq 1 ]]; then
+	echo "Can't find package $1 in PATH"
+	exit 1
+fi
+
+# if there's only one option returned by which
+if [[ $allLinkedDirectories =~ "\n" ]]; then
+	echo "$(getBinaryDirectory $allLinkedDirectories)"
+	exit 0
+fi
+
+# get only unique options
+allLinkedDirectories="$(echo -e $allLinkedDirectories | tr ' ' '\n' | sort --unique)"
+
+# run the getBinaryDirectory function
+allSourceDirectories=()
+for link in $allLinkedDirectories; do
+	allSourceDirectories+="$( getBinaryDirectory $link )\n"
+done
+allSourceDirectories="$(echo -e $allSourceDirectories | sort --unique)"
+allSourceDirectories=($allSourceDirectories)
+
+# write output
+for i in ${allSourceDirectories[*]}; do
+	echo $i
+done
+exit 0
