21d08d262037e0031e82977d25700f329ab5c931
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-07-19 20:05:36 -0400

    clean up

--- homemanager/pkgs/python/python.nix
+++ homemanager/pkgs/python/python.nix
@@ -31,19 +31,9 @@ in
       # clean up ~
       export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
     '';
-  #   programs.bash.initExtra = ''
-  #     conda_active="$(conda 2>&1 /dev/null)"
-  #     if [[ $? -eq 127 ]]; then
-  #       conda-shell
-  #       echo "activating conda"
-  #     else
-  #       echo "not activating conda"
-  #     fi
-  #   '';
 
-  #   programs.bash.logoutExtra = ''
-  #     echo "running conda deactivate"
-  #     conda deactivate
-  #   '';
+  # home.bashAliases = {
+  #   update-qstar = "condaormamaba env update --file <path>";
+  # };
 }
 # conda env update --file ~/.config/NixOS-Config/homemanager/pkgs/python/python-qstar.yml
