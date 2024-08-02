d21fd3c27dbd121bcdf6d6cdbddffbf96ec0cf35
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-08-01 21:52:45 -0400

    attempt to use sops with HM (discarded)

--- system/sops/sops.nix
+++ system/sops/sops.nix
@@ -47,3 +47,15 @@ in
   #   /run/current-system/sw/bin/systemctl start --user sops-nix
   # '';
 }
+
+# add sops to flake inputs
+# add sops HM to tai-wsl flake modules
+# add sops and age to home packages
+# mkdir --parents ~/.config/sops/age
+# sops ~/.config/NixOS-Config/secrets/secrets.yaml
+# ^ fails bc no public key is available at ~/.config/sops/age/keys.txt to decrypt
+# age-keygen --output ~/.config/sops/age/keys.txt
+# get public key with: age-keygen -y ~/.config/sops/age/keys.txt
+# add public key to .sops.yaml
+# sops --config ~/.config/NixOS-Config/system/sops/.sops.yaml [sops file to edit, e.g. ~/.config/NixOS-Config/homemanager/sops-wsl.yaml]
+# now all public keys listed in --config are able to access/decrypt [sops file]
