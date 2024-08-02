d28cfb5f4ade59d82a9572b5750c12f0212691e7
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-08-01 21:53:34 -0400

    agenix for ssh config

--- homemanager/agenix/agenix.nix
+++ homemanager/agenix/agenix.nix
@@ -1,6 +1,6 @@
-let 
-	wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKolW4+sDPYJHL0XSZ+i/XRpj/7tZRMsktKGOyAQcGAy";
-in 
+let
+  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKolW4+sDPYJHL0XSZ+i/XRpj/7tZRMsktKGOyAQcGAy";
+in
 {
-	"group_hostname.age".publicKeys = [wsl];
+  "group_hostname.age".publicKeys = [ wsl ];
 }
