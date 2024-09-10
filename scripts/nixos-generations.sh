dfcbc46232e0cab53251e578785acc5623263103
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-09-10 10:12:58 -0400

    working on nixos generations script

--- scripts/nixos-generations.sh
+++ scripts/nixos-generations.sh
@@ -8,4 +8,33 @@ currentstring="[ .[] | .generation = if .current then ( .generation| tostring ) 
 
 jqstring="$headerlist, (.[] | $attrlist) | @tsv"
 
-nixos-rebuild list-generations --json | jq -r "$dateformat" | jq -r "$currentstring" | jq -r "$jqstring" | column --table --separator $'\t'
+generations_json="$(nixos-rebuild list-generations --json)"
+
+generations_table=$( echo "$generations_json"| jq -r "$dateformat" | jq -r "$currentstring" | jq -r "$jqstring" | column --table --separator $'\t' )
+
+echo "$generations_table"
+# echo "$generations_json"
+
+# instead go through generations in reverse order
+# set initial flag of "delete before"
+# for each generation
+#   if not current
+#   if not one-previous
+#   if date<cleandate
+#       update delete_before to include this
+
+noncurrent_generations=$( echo "$generations_json" | jq "[ .[] | select(.current==false)]")
+
+if [[ "$(echo $noncurrent_generations | jq '. | length' )" == "1" ]]; then
+    echo "Only one non-current NixOS generation. Keeping for rollback."
+fi
+
+days=7
+deleteFromInt="$(date --date="$days days ago" "+%s")"
+deleteable_generations="$(echo $noncurrent_generations | jq "[ .[] | select(.date>$deleteFromInt)]")"
+
+echo "Deletable generations: "
+echo "$(echo $deleteable_generations | jq '.[] | .generation')"
+
+
+# echo $deleteable_generations
