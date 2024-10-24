5da68670724100d95029be4e86954dde5a659f03
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-10-24 14:29:39 -0400

    fix gaia

--- home-manager/pkgs/gaia.nix
+++ home-manager/pkgs/gaia.nix
@@ -4,13 +4,15 @@
   ...
 }:
 {
-  home.packages = [ pkgs.gaia ];
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "gaia";
+      runtimeInputs = [ pkgs.gaia ];
+      text = ''
+        "$STARLINK_DIR/bin/gaia/gaia.sh"
+      '';
+    })
+  ];
   home.sessionVariables."STARLINK_DIR" = "${pkgs.gaia}";
 
-  programs.bash.bashrcExtra = ''
-    # GAIA
-    shopt -u expand_aliases
-    source $STARLINK_DIR/etc/profile
-    shopt -s expand_aliases
-  '';
 }
