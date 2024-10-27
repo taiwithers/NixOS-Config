bfc9f3e4d5b76cd3575659dd125a2c1453872019
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-10-26 20:06:02 -0400

    remove steam workaround

--- nixos/main/configuration.nix
+++ nixos/main/configuration.nix
@@ -191,12 +191,6 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    package = pkgs.steam.override {
-      extraLibraries =
-        p: with p; [
-          (lib.getLib networkmanager)
-        ];
-    };
   };
 
   # gamemode - requests optimizations when running games
