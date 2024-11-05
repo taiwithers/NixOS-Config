0882bddd91e402a06607a9468c74828a9b273835
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-11-05 12:29:10 -0500

    switch bootloader back to grub

--- nixos/main/bootloader.nix
+++ nixos/main/bootloader.nix
@@ -3,17 +3,18 @@
   # boot and dual-boot options
   time.hardwareClockInLocalTime = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   # grub
-  # boot.loader.grub = {
-  #     enable = false;
-  #     efiSupport = true;
-  #     devices = [ "nodev" ];
-  #     useOSProber = true;
-  #     configurationLimit = 16;
-  # };
+  boot.loader.grub = {
+      enable = true;
+      efiSupport = true;
+      devices = [ "nodev" ];
+      useOSProber = true;
+      configurationLimit = 16;
+  };
 
   # systemd
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 16;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.configurationLimit = 16;
 }
