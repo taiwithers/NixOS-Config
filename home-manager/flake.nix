8861c2f2c4c03efa08fa6765946ba12cbf17a7ef
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-11-15 15:39:09 -0500

    re-add hm url oops

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # libraries for other inputs to follow
