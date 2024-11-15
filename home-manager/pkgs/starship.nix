a9ee922a8c8c00c01fa518073eb154f3ee1660b5
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-11-15 14:06:59 -0500

    use terminal colours for starship

--- home-manager/pkgs/starship.nix
+++ home-manager/pkgs/starship.nix
@@ -1,4 +1,4 @@
-{ pkgs, colours, ... }:
+{ pkgs, ... }:
 {
   # style strings (not case sensitive)
   # bold, italic, underline, dimmed, inverted, blink, hidden, strikethrough, <color>, fg:<color>, bg:<color>, none
@@ -7,7 +7,7 @@
     enable = true;
     enableBashIntegration = false; # do this manually with bashrcExtra since the bash integration assumes the location of .nix-profile
     enableZshIntegration = false;
-    settings = with colours.hex-hash; {
+    settings = {
       format =
         "$"
         + builtins.concatStringsSep "$" [
@@ -47,19 +47,19 @@
             threshold = 60;
             charging_symbol = "󰂉";
             discharging_symbol = "󰁿";
-            style = "${green}";
+            style = "green";
           }
           {
             threshold = 40;
             charging_symbol = "󰂇";
             discharging_symbol = "󰁻";
-            style = "${peach}";
+            style = "yellow";
           }
           {
             threshold = 10;
             charging_symbol = "󰢜";
             discharging_symbol = "󰂃";
-            style = "bold ${red}";
+            style = "bold red";
           }
         ];
       };
@@ -67,21 +67,21 @@
       character = {
         disabled = false;
         format = "$symbol ";
-        success_symbol = "[❯](bold ${green})";
-        error_symbol = "[❯](bold ${green})";
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[❯](bold green)";
       };
 
       cmd_duration = {
         disabled = false;
         min_time = 500;
         format = "took [$duration]($style) ";
-        style = "${white}";
+        style = "white";
         show_notifications = true;
         min_time_to_notify = 60;
       };
       conda = {
         disabled = false;
-        style = "${green}";
+        style = "green";
         format = ''\[[$symbol $environment]($style)\] '';
         ignore_base = false;
         symbol = "";
@@ -93,13 +93,13 @@
         truncation_length = 3;
         truncate_to_repo = true;
         truncation_symbol = ".../";
-        read_only_style = "${red}";
-        style = "${light-blue}";
+        read_only_style = "red";
+        style = "blue";
       };
       direnv = {
         format = "([$symbol$loaded/$allowed]($style) )";
         disabled = true;
-        style = "${yellow}";
+        style = "yellow";
         symbol = "direnv:";
         detect_files = [ ".envrc" ]; # filenames to trigger the direnv module
         detect_folders = [ ]; # see above
@@ -113,7 +113,7 @@
         disabled = false;
         format = "on [$symbol $branch(:$remote_branch) ]($style)";
         symbol = "";
-        style = "${purple}";
+        style = "magenta";
         ignore_branches = [
           "master"
           "main"
@@ -121,8 +121,8 @@
       };
       git_metrics = {
         disabled = false;
-        added_style = "bold ${green}"; # style for number of added lines;
-        deleted_style = "bold ${red}"; # style for number of deleted lines;
+        added_style = "bold green"; # style for number of added lines;
+        deleted_style = "bold red"; # style for number of deleted lines;
         only_nonzero_diffs = true;
         format = "(([+$added]($added_style))([-$deleted]($deleted_style)) )";
         # format = "[ ([+$added]($added_style))([-$deleted]($deleted_style) ) ]";
@@ -130,13 +130,13 @@
       };
       git_state = {
         disabled = false;
-        style = "bold ${peach}";
+        style = "bold yellow";
         format = ''\([$state( $progress_current/$progress_total)]($style)\) '';
       };
       git_status = {
         disabled = false;
         format = ''([\[$all_status$ahead_behind\]]($style) )'';
-        style = "bold ${red}";
+        style = "bold red";
         ignore_submodules = true;
         conflicted = "="; # merge conflicts;
         ahead = "⇡$count";
@@ -156,7 +156,7 @@
         ssh_symbol = "󰢹 ";
         trim_at = "";
         format = "[$ssh_symbol$hostname ]($style) ";
-        style = "bold dimmed ${green}";
+        style = "bold dimmed green";
       };
       jobs = {
         disabled = false;
@@ -164,13 +164,13 @@
         number_threshold = 2;
         format = "[$symbol$number]($style) ";
         symbol = "✦";
-        style = "bold ${light-blue}";
+        style = "bold blue";
       };
       nix_shell = {
         disabled = false;
         format = "via [$symbol( $name$state)]($style)";
         symbol = "";
-        style = "bold ${light-blue}";
+        style = "bold blue";
         impure_msg = " (impure)";
         pure_msg = "";
         unknown_msg = " (purity?)";
@@ -183,26 +183,26 @@
         bash_indicator = "bash";
         zsh_indicator = "zsh";
         format = " [$indicator]($style)";
-        style = "${white}";
+        style = "white";
       };
       status = {
         disabled = false;
         format = ''[\[$status $common_meaning\]]($style) '';
         map_symbol = false;
-        style = "${red}";
+        style = "red";
       };
       time = {
         disabled = false;
         format = " [$time]($style)";
         time_format = "%-H:%M%p";
-        style = "${grey}";
+        style = "bright-black";
       };
       username = {
         disabled = true;
         format = "[$user ]($style) ";
         show_always = false; # if false, only shows if: root, not same as login, in ssh;
-        style_user = "${red}";
-        style_root = "bold ${red}";
+        style_user = "red";
+        style_root = "bold red";
       };
     };
   };
