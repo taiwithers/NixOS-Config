7cfd382687d1c5c63f744edaef82fd334f0d7675
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-11-18 09:27:01 -0500

    update rofi config

--- home-manager/pkgs/rofi.nix
+++ home-manager/pkgs/rofi.nix
@@ -12,7 +12,7 @@
       plugins = plugins;
     }; # rofi-wayland, rofi-wayland-unwrapped
     plugins = with pkgs; [
-      rofi-calc
+      # rofi-calc
       # rofimoji
       # rofi-emoji
       # rofi-emoji-wayland
@@ -24,7 +24,7 @@
 
     cycle = false; # cycle through results
     extraConfig = rec {
-      modes = "window,drun,run,ssh,filebrowser";
+      modes = "drun,run,filebrowser";
       case-sensitive = false;
       scroll-method = 1; # 1 continuous
       normalize-match = true; # match accented and nonaccented characters
@@ -35,6 +35,8 @@
       drun-display-format = "{name} [<span weight='light' size='small'><i>({exec})</i></span>]";
       drun-show-actions = true;
       window-match-field = "all";
+
+      auto-select = true;
       # matching-negate-char = "!";
 
       window-format = "{n} - {t} [{w}]";
@@ -45,6 +47,44 @@
 
       # fixed-num-lines = true; # theme option
 
+      global-kb = false;
+
+      kb-secondary-paste = "Control+v"; # primary is selection, secondary is clipboard
+      kb-secondary-copy = "Control+c";
+      kb-clear-line = "Control+Shift+BackSpace";
+      # kb-move-front = "Control+a,Home,KP_Home";
+      # kb-move-end = "Control+e,End,KP_End";
+      kb-move-word-back = "Control+Left";
+      kb-move-word-forward = "Control+Right";
+      kb-move-char-back = "Left";
+      kb-move-char-forward = "Right";
+      kb-remove-word-back = "Control+BackSpace,Control+w";
+      kb-remove-word-forward = "Control+Delete";
+      kb-remove-char-back = "BackSpace";
+      kb-remov-char-forward = "Delete";
+      kb-remove-to-eol = "Control+k";
+      kb-remove-to-sol = "Control+u";
+      kb-accept-entry = "Return,KP_Enter";
+      kb-accept-custom = "";
+      kb-accept-custom-alt = "";
+      kb-delete-entry = "";
+      kb-accept-alt = "";
+      kb-mode-complete = "";
+      kb-row-left = "";
+      kb-row-right = "";
+      kb-element-prev = "";
+      kb-page-prev = "";
+      kb-page-next = "";
+      kb-row-select = "";
+      kb-screenshot = "";
+      kb-ellipsize = "";
+      kb-toggle-case-sensitivity = "";
+      kb-mode-next = "Tab";
+      kb-element-next = ""; # would otherwise override tab
+      kb-mode-previous = "Shift+Tab";
+      kb-row-down = "Down,Alt+j";
+      kb-row-up = "Up,Alt+k";
+      kb-cancel = "Escape,Control+bracketleft";
     };
     font = "Liberation Mono 16";
     location = "center";
