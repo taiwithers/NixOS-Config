5e15c7447570271be5954cc93c2609963fd7bc2a
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-11-19 07:37:08 -0500

    move stylix to different file

---
+++ home-manager/themeing.nix
@@ -0,0 +1,50 @@
+{
+	stylix,...
+}:{
+	imports = [stylix.homeManagerModules.stylix];
+
+	  stylix = {
+    enable = true;
+    autoEnable = false;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-ocean.yaml";
+    fonts = rec {
+        monospace.package = pkgs.intel-one-mono;
+        monospace.name = "Intel One Mono";
+        sansSerif.package = pkgs.open-sans;
+        sansSerif.name = "Open Sans";
+        serif.package = sansSerif.package;
+        serif.name = sansSerif.name;
+        sizes = {
+            applications = 12;
+            desktop = 12;
+            popups = 10;
+            terminal = 12;
+          };
+    };
+    cursor = {
+        package = pkgs.posy-cursors;
+        name = "Posy_Cursor_Black";
+        size = 32;
+    };
+    opacity = {
+      applications = 0.8;
+      desktop = 0.8;
+      popups = 0.8;
+      terminal = 0.5;
+    };
+    targets = {
+        bat.enable = true;
+        fzf.enable = true;
+        gtk.enable = false; # doesn't follow breeze theme
+        kde.enable = false; # requires wallpaper to be set
+        kitty.enable = true;
+        lazygit.enable = true;
+        neovim.enable = true;
+        rofi.enable = false; # conflicts with rofi layout styling
+        spicetify.enable = true;
+        vesktop.enable = true;
+        vscode.enable = true;
+        yazi.enable = true;
+      };
+  };
+}
