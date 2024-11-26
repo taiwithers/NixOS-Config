48e4d7cdc70e78b33f151f04d6377eced03568ec
taiwithers <59430904+taiwithers@users.noreply.github.com>
2024-11-26 15:09:03 -0500

    vesktop theme and settings

--- home-manager/pkgs/vesktop/vesktop.nix
+++ home-manager/pkgs/vesktop/vesktop.nix
@@ -5,4 +5,30 @@
   xdg.configFile."vesktop/themes/thin.theme.css".source = ./thin.theme.css;
   xdg.configFile."vesktop/themes/hide-extras.theme.css".source = ./hide-extras.theme.css;
   xdg.configFile."vesktop/themes/custom-colours.theme.css".source = ./custom-colours.theme.css;
+
+  # technically splashBackground should be tied to theme colours
+  xdg.configFile."vesktop.settings.json".text = ''
+    {
+        "minimizeToTray": "on",
+        "discordBranch": "stable",
+        "arRPC": "on",
+        "splashColor": "rgb(255, 255, 255)",
+        "clickTrayToShowHide": true,
+        "disableMinSize": true,
+        "openLinksWithElectron": false,
+        "spellCheckLanguages": [
+            "en-GB",
+            "en-CA",
+            "en"
+        ],
+        "audio": {
+            "onlySpeakers": false,
+            "onlyDefaultSpeakers": false,
+            "ignoreInputMedia": false,
+            "ignoreDevices": false
+        },
+        "splashTheming": true,
+        "splashBackground": "rgb(23, 23, 38)"
+    }
+  '';
 }
