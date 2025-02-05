0a7d4d36fce86407ff80b61d71bdbe696f4b553d
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-02-05 15:44:35 -0500

    git config updates

--- home-manager/pkgs/common-git.nix
+++ home-manager/pkgs/common-git.nix
@@ -6,11 +6,13 @@
 {
   programs.git = {
     enable = true;
-    signing.key = "${config.home.homeDirectory}/.ssh/id_ed25519_github.pub";
+    signing.key = "${config.common.userHome}/.ssh/id_ed25519_github.pub";
     signing.signByDefault = true;
     userEmail = "59430904+taiwithers@users.noreply.github.com";
     userName = "taiwithers";
     extraConfig = {
+      branch.sort = "-committerdate";
+      core.attributesfile = "${config.common.configHome}/git/attributes";
       core.autocrlf = "input";
       core.eol = "lf";
       core.fileMode = false;
@@ -23,7 +25,15 @@
       gpg.format = "ssh";
       init.defaultBranch = "main";
       pull.rebase = "true";
+      rerere.enabled = true; # record conflict resolutions
+      rerere.autoUpdate = true; 
       status.relativePaths = false;
+
+      diff.exiftool.textconv = "exiftool";
+
+    };
+    aliases = {
+      fpush = "push --force-with-lease"; # safer force push
     };
     delta = {
       enable = true;
@@ -57,5 +67,11 @@
   home.shellAliases."gitstatus" = ''
     onefetch --no-art --no-color-palette --no-title --disabled-fields authors languages churn created project pending head commits && git status --short --branch
   '';
-  home.packages = [ pkgs.onefetch ];
+  home.packages = with pkgs; [ onefetch exiftool ];
+
+
+  xdg.configFile."git/attributes".text = ''
+    *.png diff=exiftool
+    *.pdf diff=exiftool
+  '';
 }
