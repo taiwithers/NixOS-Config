3db7233ab8dffdc68134070f4d991437bf6e659c
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-02-28 13:07:39 -0500

    eza: reformat, respect gitignore by default

--- home-manager/pkgs/eza.nix
+++ home-manager/pkgs/eza.nix
@@ -1,23 +1,37 @@
-_: {
+{pkgs, ...}: {
   programs.eza = {
-    enable = true;
+    enable = true; # aliases eza to eza-with-options, and adds ls & friends
     extraOptions = [
+      # filtering
       "--long"
-      "--hyperlink"
       "--all"
-      "--group-directories-first"
+
+      # information style
       "--header"
+      "--hyperlink"
+      "--group-directories-first"
       "--time-style=iso"
-      # "--no-permissions"
+      "--icons=auto"
+      "--color=always"
+
+      # information content
       "--no-user"
+      "--git"
+      # "--no-permissions"
     ];
-    git = true;
-    icons = "auto";
-    colors = "always";
   };
 
   home.shellAliases = {
-    "ls" = "eza";
-    "tree" = "eza --tree";
+    # explicitly list those added by programs.eza.enable = true
+    # unless noted these are of the same form as .enable makes them
+    # just using long form options
+    "ls" = "eza --git-ignore"; # added gitignore
+    "ll" = "eza --long";
+    "la" = "eza --all";
+    "lt" = "eza --tree";
+    "lla" = "eza --all --long";
+
+    # new
+    "tree" = "eza --tree --git-ignore";
   };
 }
