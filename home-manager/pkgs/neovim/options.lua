ddbfab0f363340a04836b03db0539267ebd2fe30
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-05-30 15:34:23 -0400

    wsl nvim clipboard

--- home-manager/pkgs/neovim/options.lua
+++ home-manager/pkgs/neovim/options.lua
@@ -6,6 +6,27 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
 vim.g.editorconfig = true
+-- WSL2 Clipboard Sync
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = { "clip.exe" },
+    ["*"] = { "clip.exe" },
+  },
+  paste = {
+    ["+"] = {
+      "/mnt/c/Windows/System32/WindowsPowerShell/v1.0///powershell.exe",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    ["*"] = {
+      "/mnt/c/Windows/System32/WindowsPowerShell/v1.0///powershell.exe",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  },
+  cache_enabled = false,
+}
 vim.cmd("syntax on")
 
 local opt = vim.o -- equivalent to :set
