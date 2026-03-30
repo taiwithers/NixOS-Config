519c2cb5bb5b1fc4cfdc24f19065c20b31b539cf
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-03-30 16:47:51 -0400

    remove unused storage aliases

--- home-manager/pkgs/duf.nix
+++ home-manager/pkgs/duf.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.duf ];
-  home.shellAliases."df" = "echo 'Consider using duf instead, or use \\df to access df.'";
   home.shellAliases."duf" = "duf -only local";
 }
--- home-manager/pkgs/dust.nix
+++ home-manager/pkgs/dust.nix
@@ -2,7 +2,6 @@
 {
   home.packages = [ pkgs.dust ];
   home.shellAliases = {
-    "du" = "echo 'Consider using dust instead or use \\du to access du.'";
     "dust" = "dust --reverse --ignore-directory mnt";
   };
 }
