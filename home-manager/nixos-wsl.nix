18b2c803af724525ae0765dcf7333075445e0fa8
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-06-30 11:51:28 -0400

    Remove deprecated npm env var

--- home-manager/nixos-wsl.nix
+++ home-manager/nixos-wsl.nix
@@ -87,7 +87,6 @@
     NODE_REPL_HISTORY = "${config.xdg.stateHome}/node_repl_history";
     NPM_CONFIG_INIT_MODULE = "${config.xdg.configHome}/npm/config/npm-init.js";
     NPM_CONFIG_CACHE = "${config.xdg.cacheHome}/npm";
-    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
   };
 
   home.shellAliases = {
